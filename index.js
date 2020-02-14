64f57bbfcace321c3011c6bed327ea5992f73258
Dani Blackwell <bleuxgenius@gmail.com>
2020-02-13 20:19:25 -0600

    changes updated
    port made dynamic for deployment purposes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require('./server');
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => 
 console.log(`\n **API running on port ${port}`))
